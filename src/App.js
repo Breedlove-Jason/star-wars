30eb83c784ec6b65d44de6f610c0441fb67464d9
Jason Breedlove <j@jasonbreedlove.com>
2022-07-06 08:35:15 -0700

    refactored code
    removed unnecessary and unutilized code in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
-import axios from 'axios';
 import Characters from "./components/characters/Characters.component";
 
 function App() {
     const [characters, setCharacters] = useState([]);
-    const [home, setHome] = useState([]);
-    const [type, setType] = useState([]);
-    const [person, setPerson] = useState([]);
-    const [height, setHeight] = useState([]);
-    const [mass, setMass] = useState([]);
-    const [birth, setBirth] = useState([]);
 
 
     useEffect(() => {
@@ -19,38 +12,6 @@ function App() {
                 "https://swapi.dev/api/people/?format=json"
             );
             setCharacters(characterResponse.data.results);
-
-            const world = [];
-            const species = [];
-            const person = [];
-            const height = [];
-            const mass = [];
-            const birth = [];
-            for (const element of characters) {
-                const nameResponse = element.name
-                person.push(nameResponse);
-                const heightResponse = element.height
-                height.push(heightResponse);
-                const massResponse = element.mass
-                mass.push(massResponse);
-                const birthResponse = element.birth_year
-                birth.push(birthResponse);
-                const homeWorldResponse = await axios.get(
-                    element.homeworld)
-                world.push(homeWorldResponse.data.name)
-
-                const speciesResponse = await axios.get(
-                    element.species)
-                species.push(speciesResponse.data.name)
-                setPerson(person);
-                setHeight(height);
-                setMass(mass);
-                setBirth(birth);
-                setHome(world);
-                setType(species);
-
-            }
-
 
         }
 
