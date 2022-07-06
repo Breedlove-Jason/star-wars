f6905a3862b428a0162f6b3d538f3db9e6a9a1d2
Jason Breedlove <j@jasonbreedlove.com>
2022-07-06 08:37:58 -0700

    replaced axios dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Characters from "./components/characters/Characters.component";
 
 function App() {
